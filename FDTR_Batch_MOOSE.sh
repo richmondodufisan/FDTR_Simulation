61b1a67c9080080e362657ed220c32aa90536ea4
Alesanmi Richmond Odufisan <vtw1026@quser32.quest.it.northwestern.edu>
2023-11-05 13:10:56 -0600

    added explicit directory for executable in batch script

--- FDTR_Batch_MOOSE.sh
+++ FDTR_Batch_MOOSE.sh
@@ -5,12 +5,12 @@
 #SBATCH --ntasks-per-node=25 ## how many cpus or processors do you need on each computer
 #SBATCH --time=2:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
 #SBATCH --mem-per-cpu=1G ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
-#SBATCH --job-name=-1560  ## When you run squeue -u NETID this is how you can identify the job
+#SBATCH --job-name=020  ## When you run squeue -u NETID this is how you can identify the job
 
 
-script_name="FDTR_input_theta_0_freq_6e6_x0_-15_v1.i"
+script_name="FDTR_input_theta_0_freq_2e6_x0_0_v1.i"
 
 #moose_exec.sh ../purple-opt -i ${script_name} --mesh-only
 #moose_exec.sh ../purple-opt -i ${script_name}
 
-mpiexec -np ${SLURM_NTASKS} singularity exec -B /projects:/projects -B /projects/p32089/singularity/moose/moose:/opt/moose /projects/p32089/singularity/moose_latest.sif ../purple-opt -i ${script_name}
+mpiexec -np ${SLURM_NTASKS} singularity exec -B /projects:/projects -B /projects/p32089/singularity/moose/moose:/opt/moose /projects/p32089/singularity/moose_latest.sif /projects/p32089/MOOSE_Applications/purple/purple-opt -i ${script_name}
