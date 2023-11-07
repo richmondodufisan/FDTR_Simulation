814f88ddc8667175c6ab7f23c6c2bf40871f7807
Alesanmi Richmond Odufisan <vtw1026@quser31.quest.it.northwestern.edu>
2023-11-07 17:04:12 -0600

    modified to move old data to scratch directory

--- FDTR_Batch_MOOSE.sh
+++ FDTR_Batch_MOOSE.sh
@@ -3,14 +3,14 @@
 #SBATCH --partition=short  ### PARTITION (buyin, short, normal, etc)
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=25 ## how many cpus or processors do you need on each computer
-#SBATCH --time=2:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --time=4:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
 #SBATCH --mem-per-cpu=1G ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
-#SBATCH --job-name=020  ## When you run squeue -u NETID this is how you can identify the job
+#SBATCH --job-name=15100  ## When you run squeue -u NETID this is how you can identify the job
 
 
-script_name="FDTR_input_theta_0_freq_2e6_x0_0_v2.i"
+script_name="FDTR_input_theta_0_freq_10e6_x0_15_v2.i"
 
 #moose_exec.sh ../purple-opt -i ${script_name} --mesh-only
 #moose_exec.sh ../purple-opt -i ${script_name}
 
-mpiexec -np ${SLURM_NTASKS} singularity exec -B /projects:/projects -B /projects/p32089/singularity/moose/moose:/opt/moose /projects/p32089/singularity/moose_latest.sif /projects/p32089/MOOSE_Applications/purple/purple-opt -i ${script_name}
+mpiexec -np ${SLURM_NTASKS} singularity exec -B /projects:/projects -B /scratch:/scratch -B /projects/p32089/singularity/moose/moose:/opt/moose /projects/p32089/singularity/moose_latest.sif /projects/p32089/MOOSE_Applications/purple/purple-opt -i ${script_name}
--- Parameter_Sweep_HPC.sh
+++ Parameter_Sweep_HPC.sh
@@ -17,10 +17,11 @@ function check_squeue() {
 
 
 # Set the maximum number of times to submit the batch job
-n_iterations=60
+#n_iterations=60
+n_iterations=15
 
 # Set the number of periods each job/sweep should solve for
-n_periods_per_job=0.5
+n_periods_per_job=1.0
 
 # Initial timestep
 start_val=0.0
@@ -38,17 +39,17 @@ first_period=$n_periods_per_job
 
 # Define the range of values you want to loop over
 
-x0_vals_num=("0")
+#x0_vals_num=("0")
 
-freq_vals_num=("1e6" "2e6")
-
-theta_vals_num=("0")
-
-#x0_vals_num=("-15" "-10" "-9" "-8" "-7" "-6" "-5" "-4" "-3" "-2" "-1" "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "15")
-
-#freq_vals_num=("1e6" "2e6" "4e6" "6e6" "10e6")
+#freq_vals_num=("1e6" "2e6")
 
 #theta_vals_num=("0")
+
+x0_vals_num=("-15" "-10" "-9" "-8" "-7" "-6" "-5" "-4" "-3" "-2" "-1" "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "15")
+
+freq_vals_num=("1e6" "2e6" "4e6" "6e6" "10e6")
+
+theta_vals_num=("0")
 
 
 # Loop over values
@@ -107,7 +108,7 @@ for x0_val_num in "${x0_vals_num[@]}"; do
 			sed -E -i "s/(#SBATCH --job-name=)[^[:space:]]+/\1${x0_val_num}${freq_noexp}${theta_val_num}/" "FDTR_Batch_MOOSE.sh"
 
 			# Submit job
-			sbatch FDTR_Batch_MOOSE.sh
+			#sbatch FDTR_Batch_MOOSE.sh
 		done
 	done
 done
@@ -134,10 +135,12 @@ while [ $submission_count -lt $n_iterations ]; do
     if check_squeue; then
 		echo "No jobs in the queue. Submitting batch job script..."
 
+		mv *.e /scratch/vtw1026/		
+
 		# Delete mesh files from older submission
                 if [ $submission_count -gt 1 ]; then
                         older_submission=$((submission_count - 1))
-                	rm *v${older_submission}*e
+                	rm /scratch/vtw1026/*v${older_submission}*e
                 fi
 		
 
@@ -146,7 +149,7 @@ while [ $submission_count -lt $n_iterations ]; do
 		former_sim_ver="v${o_ver}"
 		new_sim_ver="v${n_ver}"
 		
-		rm rm *${former_sim_ver}*i
+		rm *${former_sim_ver}*i
 
 		o_stop=$(echo "$o_start + $n_periods_per_job" | bc -l)
 		n_stop=$(echo "$o_stop + $n_periods_per_job" | bc -l)
@@ -179,8 +182,9 @@ while [ $submission_count -lt $n_iterations ]; do
 					sed -i "s/\(x0_val\s*=\s*\)[0-9.eE+-]\+/\1$x0_val_num/g" "$new_filename"
 					
 					# Replace the mesh in the MOOSE script
+					scratch_path="\/scratch\/vtw1026\/"
 					former_sim_output="${init_filename}_theta_${theta_val_num}_freq_${freq_val_num}_x0_${x0_val_num}_${former_sim_ver}_out.e"
-					sed -i "0,/file = [^ ]*/s/file = [^ ]*/file = \"$former_sim_output\"/" "$new_filename"
+					sed -i "0,/file = [^ ]*/s/file = [^ ]*/file = \"$scratch_path$former_sim_output\"/" "$new_filename"
 					
 					
 					############# Replacing end and start periods #############
